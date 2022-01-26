df7180186aab3503ba902dafe25f32760b0c8a31
Olha Fastiuk <olhafastiuk@gmail.com>
2022-01-26 13:59:42 +0200

    Fix path

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
-import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { Fragment, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { NavBar } from "./components/Navigation";
 import Settings from "./pages/Settings";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import tasks from "./components/reducers.js/tasks";
-
-function saveToLocalStorage(state) {
-  try {
-    const serialisedState = JSON.stringify(state);
-    localStorage.setItem("tasks", serialisedState);
-  } catch (e) {
-    console.warn(e);
-  }
-}
-
-function loadFromLocalStorage() {
-  try {
-    const serialisedState = localStorage.getItem("tasks");
-    if (serialisedState === null) return undefined;
-    return JSON.parse(serialisedState);
-  } catch (e) {
-    console.warn(e);
-    return undefined;
-  }
-}
-
-const store = createStore(tasks, loadFromLocalStorage());
-store.subscribe(() => saveToLocalStorage(store.getState()));
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <Fragment>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,15 +4,43 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import tasks from "./components/reducers.js/tasks";
+import { HashRouter } from "react-router-dom";
+
+function saveToLocalStorage(state) {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem("tasks", serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serialisedState = localStorage.getItem("tasks");
+    if (serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  } catch (e) {
+    console.warn(e);
+    return undefined;
+  }
+}
+
+const store = createStore(tasks, loadFromLocalStorage());
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
